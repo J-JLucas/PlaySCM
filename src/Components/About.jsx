0d059ae667a9baac84bca934f9e3b522b3bb9179
Josh Lucas <90178749+J-JLucas@users.noreply.github.com>
2023-08-31 21:05:43 +0000

    Updated wesite pictures

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/beach2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Another picture of a beach";
 
 /**
  * Sort description that expands on your title on the Home component.
