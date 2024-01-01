df46d89e524c4605c1d847fed2904be670c0b6df
Josh Lucas <90178749+J-JLucas@users.noreply.github.com>
2024-01-01 18:24:26 +0000

    adjusted about page to make mobile friendly

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,6 @@
-
-
 import React from "react";
 import image from "../images/blankSky.png";
+import "./About.css";
 
 const imageAltText = "Starcraft Skybox";
 
@@ -24,41 +23,24 @@ const skillsList = [
 const detailOrQuote =
   "SCM was built in Unreal5 over the summer of 2023. Special thanks to my friend Hayden for his Blender expertise, without him this project would have never gotten started, and my brother Nick for adding that extra kick to the weapon SFX. Also thank you to my playtesters for taking the time to provide valueable feedback. The project is currently on hiatus as I am busy with school, but I hope to continue working on it in the future.";
 
-const About = () => {
-  return (
-    <section className="padding" id="about">
-      <img className="background" src={image} alt={imageAltText} />
-      <div
-        style={{
-          backgroundColor: "rgba(0,0,0,0.75)",
-          color: "rgb(197, 217, 255)",
-          width: "50%",
-          padding: "4rem",
-          margin: "3rem auto",
-          textAlign: "center",
-        }}
-      >
-        <h2>STARCRAFT: MARINE</h2>
-        <p className="large">{description}</p>
-        <hr />
-        <ul
-          style={{
-            textAlign: "left",
-            columns: 2,
-            fontSize: "1.25rem",
-            margin: "2rem 3rem",
-            gap: "3rem",
-          }}
-        >
-          {skillsList.map((skill) => (
-            <li key={skill}>{skill}</li>
-          ))}
-        </ul>
-        <hr />
-        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
-      </div>
-    </section>
-  );
-};
-
-export default About;
+  const About = () => {
+    return (
+      <section className="padding" id="about">
+        <img className="background" src={image} alt={imageAltText} />
+        <div className="about-section">
+          <h2>STARCRAFT: MARINE</h2>
+          <p className="large">{description}</p>
+          <hr />
+          <ul className="skills-list">
+            {skillsList.map((skill) => (
+              <li key={skill}>{skill}</li>
+            ))}
+          </ul>
+          <hr />
+          <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+        </div>
+      </section>
+    );
+  };
+  
+  export default About;
