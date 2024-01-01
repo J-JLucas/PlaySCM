5dcc87421a396b55904cbb00b6530b019414455f
Josh Lucas <90178749+J-JLucas@users.noreply.github.com>
2024-01-01 19:19:12 +0000

    widen about textlines on mobile and remove arrow from homescreen

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,9 +18,11 @@ const Home = () => {
   return (
     <section id="home" className="min-height" style={{ backgroundColor: isMobileDevice ? 'black' : 'transparent' }}>
       {!isMobileDevice && <img className="background" src={image} alt="" />}
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
-      </div>
+      {!isMobileDevice && (
+        <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
+          <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        </div>
+      )}
       <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
         <iframe 
           title="YouTube video player"
